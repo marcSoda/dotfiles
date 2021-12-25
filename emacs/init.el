c891482cbda3c1a567cdb2e68e5cacf8a1760b18
marc.soda <m@soda.fm>
2021-12-24 20:54:39 -0500

    updated emacs structure

--- emacs/init.el
+++ emacs/init.el
@@ -53,20 +53,8 @@
 ;;DOOM-THEME
 (use-package doom-themes
   :config
-  ;; Global settings (defaults)
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
   (load-theme 'doom-acario-dark t)
-  ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
-  ;; Enable custom neotree theme (all-the-icons must be installed!)
-  (doom-themes-neotree-config)
-  ;; or for treemacs users
-  (defvar doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
-  (doom-themes-treemacs-config)
-  ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config)
-  (set-face-attribute 'default nil :height 185 :weight 'bold)
+  (set-face-attribute 'default nil :height 195 :weight 'bold)
   (set-background-color "#121212")
   (set-face-attribute 'hl-line nil :background "#3e4446"))
 
@@ -74,7 +62,6 @@
 (use-package doom-modeline
   :init (doom-modeline-mode 1)
   :config (set-face-attribute 'mode-line nil :height 160))
-
 
 ;;DOCKER-TRAMP
 (use-package docker-tramp)
@@ -181,9 +168,9 @@
 (use-package which-key
   :config (which-key-mode))
 
+;;LOAD FILES
 (load-file (expand-file-name "mu4e.el" user-emacs-directory))
 (load-file (expand-file-name "keybindings.el" user-emacs-directory))
-(load-file (expand-file-name "theme.el" user-emacs-directory))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
